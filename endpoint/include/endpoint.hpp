2dca34afe391b03c99a37966fb2bd3010d144dbb
Kirill Chernigovskiy <138433730+kcherno@users.noreply.github.com>
2023-12-03 20:26:50 +0800

    Fix endpoint header, modify constructors

--- endpoint/include/endpoint.hpp
+++ endpoint/include/endpoint.hpp
@@ -23,8 +23,16 @@ namespace endpoint {
 class endpoint::endpoint_interface {
 public:
     using address_t = struct ::sockaddr;
-    
+
     constexpr endpoint_interface() noexcept : identifier {-1} {}
+    
+    explicit constexpr endpoint_interface(int id)
+    {
+	if (id < 0)
+	    throw std::runtime_error {::strerrorname_np(errno)};
+
+	identifier = id;
+    }
     
     endpoint_interface(const endpoint_interface&) = delete;
     
@@ -47,6 +55,29 @@ public:
     virtual ~endpoint_interface()
     {
 	close();
+    }
+
+    virtual void create()  = 0;
+    virtual void bind()    = 0;
+    virtual void connect() = 0;
+    
+    virtual const address_t* address() const noexcept = 0;
+    
+    
+    void listen(int backlog) const
+    {
+	if (::listen(identifier, backlog) == -1)
+	    throw std::runtime_error {::strerrorname_np(errno)};
+    }
+
+    void close()
+    {
+	if (identifier >= 0) {
+	    if (::close(identifier) == -1)
+		throw std::runtime_error {::strerrorname_np(errno)};
+
+	    identifier = -1;
+	}
     }
 
     constexpr int fd() const noexcept
@@ -72,29 +103,13 @@ protected:
 	if (::bind(identifier, addr, addrlen) == -1)
 	    throw std::runtime_error {::strerrorname_np(errno)};
     }
-	
+    
     void connect(const address_t* addr, socklen_t addrlen) const
     {
 	if (::connect(identifier, addr, addrlen) == -1)
 	    throw std::runtime_error {::strerrorname_np(errno)};
     }
-
-    void listen(int backlog) const
-    {
-	if (::listen(identifier, backlog) == -1)
-	    throw std::runtime_error {::strerrorname_np(errno)};
-    }
-
-    void close()
-    {
-	if (identifier >= 0) {
-	    if (::close(identifier) == -1)
-		throw std::runtime_error {::strerrorname_np(errno)};
-
-	    identifier = -1;
-	}
-    }
-
+    
 private:
     int identifier;
 };
@@ -104,53 +119,10 @@ public:
     using address_t = struct ::sockaddr_in;
 
     constexpr net_endpoint() noexcept : endpoint_interface() {}
-
-    net_endpoint(const net_endpoint&) = delete;
-
-    constexpr net_endpoint(net_endpoint&& endpoint) noexcept
-    {
-	endpoint_interface::operator=(std::move(endpoint));
-    }
-
-    net_endpoint& operator=(const net_endpoint&) = delete;
-
-    constexpr net_endpoint& operator=(net_endpoint&& endpoint) noexcept
-    {
-	endpoint_interface::operator=(std::move(endpoint));
-	
-	return *this;
-    }
     
-    virtual ~net_endpoint() = default;
-    
-    virtual void create() = 0;
+    explicit constexpr net_endpoint(int id) : endpoint_interface(id) {}
 
-    address_t bind(std::string_view sp, uint16_t port) const
-    {
-	address_t taddr = init_addr(sp, port);
-
-	using basic_addr_t = endpoint_interface::address_t;
-
-	endpoint_interface::bind(reinterpret_cast<const basic_addr_t*>(&taddr),
-				 sizeof(taddr));
-
-	return taddr;
-    }
-
-    address_t connect(std::string_view sp, uint16_t port) const
-    {
-	address_t taddr = init_addr(sp, port);
-
-	using basic_addr_t = endpoint_interface::address_t;
-
-	endpoint_interface::connect(reinterpret_cast<const basic_addr_t*>(&taddr),
-				    sizeof(taddr));
-
-	return taddr;
-    }
-
-private:
-    address_t init_addr(std::string_view sp, uint16_t port) const
+    net_endpoint(int id, std::string_view sp, uint16_t port) : endpoint_interface(id)
     {
 	address_t taddr;
 	
@@ -160,18 +132,71 @@ private:
 	taddr.sin_port   = ::htons(port);
 	
 	if (::inet_aton(sp.data(), &taddr.sin_addr) == 0)
-	    throw std::runtime_error {"IPv4 address is invalid"};
-	
-	return taddr;
+	    throw std::runtime_error {"Invalid IPv4 address format"};
+
+	::memcpy(&addr, &taddr, sizeof(addr));
     }
+
+    net_endpoint(const net_endpoint&) = delete;
+
+    constexpr net_endpoint(net_endpoint&& endpoint) noexcept
+    {
+	net_endpoint::operator=(std::move(endpoint));
+    }
+
+    net_endpoint& operator=(const net_endpoint&) = delete;
+
+    constexpr net_endpoint& operator=(net_endpoint&& endpoint) noexcept
+    {
+	endpoint_interface::operator=(std::move(endpoint));
+
+	::memcpy(&addr, &endpoint.addr, sizeof(addr));
+	::memset(&endpoint.addr, 0, sizeof(endpoint.addr));
+	
+	return *this;
+    }
+    
+    virtual ~net_endpoint() = default;
+
+    void bind() override final
+    {
+	using basic_address_t = endpoint_interface::address_t;
+	
+	endpoint_interface::bind(reinterpret_cast<const basic_address_t*>(&addr),
+				 sizeof(addr));
+    }
+
+    void connect() override final
+    {
+	using basic_address_t = endpoint_interface::address_t;
+	
+	endpoint_interface::connect(reinterpret_cast<const basic_address_t*>(&addr),
+				    sizeof(addr));
+
+	// TODO(#1): add the ability to get the socket address
+    }
+    
+    const endpoint_interface::address_t* address() const noexcept override final
+    {	
+	return reinterpret_cast<const endpoint_interface::address_t*>(&addr);
+    }
+    
+private:
+    address_t addr;
 };
 
 class endpoint::tcp_endpoint final : public endpoint::net_endpoint {
 public:
     using address_t = net_endpoint::address_t;
-    
+
     constexpr tcp_endpoint() noexcept : net_endpoint() {}
     
+    explicit constexpr tcp_endpoint(int id) noexcept : net_endpoint(id) {}
+
+    tcp_endpoint(std::string_view sp, uint16_t port) :
+	net_endpoint(::socket(AF_INET, SOCK_STREAM, 0), sp, port)
+    {}
+	
     tcp_endpoint(const tcp_endpoint&) = delete;
 
     constexpr tcp_endpoint(tcp_endpoint&& endpoint) noexcept
